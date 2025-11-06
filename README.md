baafb9dd48dcc6531f940a55a70d48ecae122c6a
Rolf <rolf.schellenberger@fastnedcharging.com>
2025-11-06 08:42:29 +0100

    Quest 1, part 1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Everybody codes 2025
-https://everybody.codes/event/2025
+https://everybody.codes/event/2025/quests
 
 ## Convert assignments to markdown
 https://www.convertsimple.com/convert-html-to-markdown/
--- src/main/kotlin/com/rolf/quest01/Day01.kt
+++ src/main/kotlin/com/rolf/quest01/Day01.kt
@@ -1,6 +1,7 @@
 package com.rolf.quest01
 
 import com.rolf.Day
+import com.rolf.util.splitLine
 
 fun main() {
     Solve().run()
@@ -8,7 +9,18 @@ fun main() {
 
 class Solve : Day() {
     override fun solve1(lines: List<String>) {
-
+        val names = splitLine(lines[0], ",")
+        val instructions = splitLine(lines[2], ",")
+        var pointer = 0
+        instructions.forEach {
+            val direction = it[0]
+            val amount = it.substring(1).toInt()
+            when (direction) {
+                'L' -> pointer = maxOf(0, pointer - amount)
+                'R' -> pointer = minOf(names.size - 1, pointer + amount)
+            }
+        }
+        println(names[pointer])
     }
 
     override fun solve2(lines: List<String>) {
